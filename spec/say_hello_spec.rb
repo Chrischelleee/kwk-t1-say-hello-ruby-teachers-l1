d530dbe4f63682137a9303c3fc862c5346b64924
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-01 19:42:21 +0000

    automatically backed up by learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,7 +1,7 @@
 require_relative './spec_helper'
 def say_hello("Sofie")
   puts "Hello #{say_hello}"
-end 
+end
 describe "say_hello" do
 
   it 'accepts an argument of a name and prints out Hello with that Name' do
